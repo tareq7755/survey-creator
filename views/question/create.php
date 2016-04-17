71cc3f8133883c60652cb450e2af7728f0c3fa72
Tareq <tareq.al-abbas@live.com>
2016-04-17 21:56:07 +0300

    questions controller + phase 2 of survey creation

---
+++ views/question/create.php
@@ -0,0 +1,46 @@
+<?php
+die($surveyId);
+$this->title = 'My Yii Application';
+echo $this->render('../partials/siteHeader');
+?>
+
+<div class="main-container">
+    <?= $this->render('../partials/sideNav'); ?>
+    <div class="content-container">
+        <div class="page-dashboard">
+            <div class="row">
+                <div class="col m12">
+                    <div class="panel">
+                        <div class="questions-form-container multiple-choice-question">
+                            <div class="input-field question-wrapper">
+                                <textarea form="questions-form" name = "question-body" id="textarea1" class="materialize-textarea"></textarea>
+                                <label for="textarea1" class="questionNum"></label>
+                            </div>
+                            <div class="input-field option-wrapper">
+                                <input type="text" class="validate" name="question-option">
+                                <label class="optionNum"></label>
+                            </div>
+                            <div class="input-field option-wrapper">
+                                <input type="text" class="validate" name="question-option">
+                                <label class="optionNum"></label>
+                            </div>
+                            <div class="add-option"><a href="#">Add option</a></div>
+                        </div>
+                        <div class="submit-btn-wrapper">
+                            <div id="submit-questions" class="btn waves-effect waves-light right submit-btn">Submit</div>
+                        </div>                            
+                        <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
+                        <input id="survey-id" type="hidden" value="<?= $surveyId; ?>" />
+
+                        <div class="add-questiontype-wrapper">
+                            <div class="add-question" id="add-question"><a href="#">Add question</a></div>
+                            <div class="add-essay" id="add-essay"><a href="#">Add essay</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
